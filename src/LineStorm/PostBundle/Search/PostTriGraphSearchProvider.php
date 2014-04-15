80dd52f84bb27f87db9d4c80bcd538d7a0283403
Andy Thorne <contrabandvr@gmail.com>
2014-04-15 17:34:32 +0000

    Updated Post Search provider and templates

--- src/LineStorm/PostBundle/Search/PostTriGraphSearchProvider.php
+++ src/LineStorm/PostBundle/Search/PostTriGraphSearchProvider.php
@@ -2,22 +2,79 @@
 
 namespace LineStorm\PostBundle\Search;
 
+use Andy\PortfolioBundle\Entity\BlogPost;
+use Doctrine\ORM\QueryBuilder;
 use LineStorm\SearchBundle\Search\Provider\TriGraphSearchProvider;
 use LineStorm\SearchBundle\Search\SearchProviderInterface;
 
+/**
+ * Class PostTriGraphSearchProvider
+ *
+ * @package LineStorm\PostBundle\Search
+ */
 class PostTriGraphSearchProvider extends TriGraphSearchProvider implements SearchProviderInterface
 {
 
+    /**
+     * @inheritdoc
+     */
     public function getName()
     {
         return 'post';
     }
 
+    /**
+     * @inheritdoc
+     */
+    public function queryBuilder(QueryBuilder $qb, $alias)
+    {
+        $qb->addSelect('c')
+            ->join($alias.'.category', 'c');
+
+        $qb->addSelect('ta')
+            ->join($alias.'.tags', 'ta');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRoute($entity)
+    {
+        if($entity instanceof BlogPost)
+        {
+            return array(
+                'linestorm_cms_post',
+                array(
+                    'category' => $entity->getCategory()->getName(),
+                    'id'       => $entity->getId(),
+                    'slug'     => $entity->getSlug(),
+                )
+            );
+        }
+        elseif(is_array($entity))
+        {
+            return array(
+                'linestorm_cms_post',
+                array(
+                    'category' => $entity['category']['name'],
+                    'id'       => $entity['id'],
+                    'slug'     => $entity['slug'],
+                )
+            );
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function getTriGraph()
     {
         return 'trigraph_post';
     }
 
+    /**
+     * @inheritdoc
+     */
     public function getIndexFields()
     {
         return array(
